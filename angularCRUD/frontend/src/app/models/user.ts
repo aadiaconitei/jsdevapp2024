cfb61c4b3cfa4c0106568a5558bea4f887b00c35
aadiaconitei <aadiaconitei@yahoo.com>
2024-06-05 17:24:26 +0300

    curs3

---
+++ angularCRUD/frontend/src/app/models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  nume: string;
+  prenume: string;
+  datanastere: Date; //string
+  email: string;
+  telefon: string;
+  dataadaugare?: Date;
+  poza?: string;
+  actiune?: string;
+  users?: any;
+}
